56dad46a3fbcd119978dc64c64b2c589cc8e6a6d
saja967 <alnzysjy062@gmail.com>
2026-07-12 11:44:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.post("/webhook", async (req, res) => {
       }
 
       await axios.post(
-        https://graph.facebook.com/v22.0/${PHONE_NUMBER_ID}/messages,
+        `https://graph.facebook.com/v22.0/${PHONE_NUMBER_ID}/messages`,
         {
           messaging_product: "whatsapp",
           to: from,
